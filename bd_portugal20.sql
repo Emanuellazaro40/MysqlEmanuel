d0237dfc23c621b97c71a0edc84f1168cdd78ce4
Emanuellazaro40 <164494230+Emanuellazaro40@users.noreply.github.com>
2024-10-16 16:27:57 +0100

    Add files via upload

---
+++ bd_portugal20.sql
@@ -0,0 +1,14 @@
+insert into Cidades (
+nome_cidade,
+codigo_postal) values 
+('Lisboa', '1000'),
+('Porto', '4150'),
+('Vila Nova de Gaia', '4430'),
+('Amadora', '1500'),
+('Braga', '4700'),
+('Funchal', '9000'),
+('Coimbra', '3030'),
+('Setúbal', '2900'),
+('Almada', '2800'),
+('Alguva_Cacem', '2735'),
+('Queluz', '4800');
